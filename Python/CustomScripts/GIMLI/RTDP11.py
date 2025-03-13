2650e6c50d082ba5809ef36b39d2b8e47882cb89
Benjamin Lindblom <90192570+AdStellae@users.noreply.github.com>
2025-03-13 17:17:31 +0200

    Add experimental version of main script

--- Python/CustomScripts/GIMLI/RTDP11.py
+++ Python/CustomScripts/GIMLI/RTDP11.py
@@ -1,3 +1,5 @@
+'''Experimental version'''
+
 # TODO Attempt to make the clustering algorithm distance-agnostic.
 # TODO Verify that the overlay thread is working after breaking it down into smaller helper functions
 
